4c4a638b04815ec0eae46201deac4202ff21a86a
Phillipp Stengel <phillipp.stengel@bit-run.de>
2018-09-15 18:46:29 +0200

    Scrap fixed

--- backend/scrap.py
+++ backend/scrap.py
@@ -2,6 +2,80 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import predict_ingredients
+
+unitConversionTable = {"cup": 0.24, #liter
+        "cups":0.24,
+        "tablespoon": 0.0143, #kilogramm
+        "tablespoons": 0.0143,
+        "teaspoon": 0.0048, #kilogramm
+        "teaspoon": 0.0048,
+        "quart": 1.0, #liter
+        "quarts": 1.0,
+        "ounce": 0.03, #kilogramm
+        "ounces": 0.03,
+        "slice": 0.05,
+        "slices": 0.05,
+        "gramms": 0.001,
+        "gramm": 0.001,
+        "kilogramm": 1.0,
+        "kilogramms": 1.0,
+        "gramms": 0.001,
+        "gramm": 0.001,
+        "millilitres": 0.001,
+        "milliliters": 0.001,
+        "l": 1.0,
+        "ml": 0.001,
+        "g": 0.001,
+        "kg": 1.0,
+        "pounds": 0.5,
+        "pounds": 0.5,
+        "pounds": 0.5,
+        "pound": 0.5}
+
+co2footprintTable = {"Beef":27.76,
+    "Cheese slices":8.29,
+    "Cheese spread":6.20,
+    "Chicken":6.82,
+    "Chocolate":14.67,
+    "Chocolate milk":4.65,
+    "Corn":0.19,
+    "Dried Beef": 27.76,
+    "Dry Pasta":1.49,
+    "Fresh ravioli with meat":10.25,
+    "Fresh ravioli with spinach and ricotta":3.06,
+    "Gruyére":8.29,
+    "Ham":10.76,
+    "Lentils":0.78,
+    "Low fat organic milk":1.11,
+    "Non-free range eggs":3.94,
+    "Pancetta":10.76,
+    "Pork":10.76,
+    "Pork sausages":10.76,
+    "Quorn":0.19,
+    "Red kidney beans":0.78,
+    "Rice milk":0.26,
+    "Risotto rice":1.03,
+    "Sliced beef":27.76,
+    "Sliced chicken":6.8,
+    "Smoked cheese":6.20,
+    "Spaghetti Carbonara pre-made meal":3.44,
+    "Tofu":3.23,
+    "UHT milk":1.31,
+    "Vegetarian salami":0.19,
+    "Wholegrain dry pasta":1.49,
+    "Lamb":25.44,
+    "Butter":11.52,
+    "Turkey":5.9,
+    "Rabbit":3.61,
+    "Cod":3.49,
+    "Eggs":3.4,
+    "Insect":2.3,
+    "Whole Milk":1.4,
+    "Wheat":0.51,
+    "Peas":0.6,
+    "Barley":0.52,
+    "Chickpeas":0.67}
+
 def parse(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -31,7 +105,7 @@ def parse(url):
                 quantity = ingredientsText.split(" ")[0]
                 unit = None
                 name = ""
-            if ingredientsText.split(" ")[1+offset] in scrap.unitConversionTable:
+                if ingredientsText.split(" ")[1+offset] in unitConversionTable:
                     unit =  ingredientsText.split(" ")[1+offset]
                     offset = offset + 1
                 name = " ".join(ingredientsText.split(" ")[1+offset:])
@@ -63,14 +137,14 @@ def classify(ingredient, unit, quantity):
         quantity = float(quantity)
 
 
-    if predictedIngredient in scrap.co2footprintTable:
+    if predictedIngredient in co2footprintTable:
         mass = -1;
-        if unit in scrap.unitConversionTable:
-            mass = quantity*scrap.unitConversionTable[unit]
+        if unit in unitConversionTable:
+            mass = quantity*unitConversionTable[unit]
 
         #calculate footprint
         if mass > 0:
-            co2Footprint =scrap. co2footprintTable[predictedIngredient]*mass
+            co2Footprint = co2footprintTable[predictedIngredient]*mass
         else:
             co2Footprint = 0.123456789
     else:
@@ -82,76 +156,3 @@ def classify(ingredient, unit, quantity):
             water=waterFootprint,
             energy=energyFootprint
     )
-
-    unitConversionTable = {"cup": 0.24, #liter
-         "cups":0.24,
-         "tablespoon": 0.0143, #kilogramm
-         "tablespoons": 0.0143,
-         "teaspoon": 0.0048, #kilogramm
-         "teaspoon": 0.0048,
-         "quart": 1.0, #liter
-         "quarts": 1.0,
-         "ounce": 0.03, #kilogramm
-         "ounces": 0.03,
-         "slice": 0.05,
-         "slices": 0.05,
-         "gramms": 0.001,
-         "gramm": 0.001,
-         "kilogramm": 1.0,
-         "kilogramms": 1.0,
-         "gramms": 0.001,
-         "gramm": 0.001,
-         "millilitres": 0.001,
-         "milliliters": 0.001,
-         "l": 1.0,
-         "ml": 0.001,
-         "g": 0.001,
-         "kg": 1.0,
-         "pounds": 0.5,
-         "pounds": 0.5,
-         "pounds": 0.5,
-         "pound": 0.5}
-
-    co2footprintTable = {"Beef":27.76,
-        "Cheese slices":8.29,
-        "Cheese spread":6.20,
-        "Chicken":6.82,
-        "Chocolate":14.67,
-        "Chocolate milk":4.65,
-        "Corn":0.19,
-        "Dried Beef": 27.76,
-        "Dry Pasta":1.49,
-        "Fresh ravioli with meat":10.25,
-        "Fresh ravioli with spinach and ricotta":3.06,
-        "Gruyére":8.29,
-        "Ham":10.76,
-        "Lentils":0.78,
-        "Low fat organic milk":1.11,
-        "Non-free range eggs":3.94,
-        "Pancetta":10.76,
-        "Pork":10.76,
-        "Pork sausages":10.76,
-        "Quorn":0.19,
-        "Red kidney beans":0.78,
-        "Rice milk":0.26,
-        "Risotto rice":1.03,
-        "Sliced beef":27.76,
-        "Sliced chicken":6.8,
-        "Smoked cheese":6.20,
-        "Spaghetti Carbonara pre-made meal":3.44,
-        "Tofu":3.23,
-        "UHT milk":1.31,
-        "Vegetarian salami":0.19,
-        "Wholegrain dry pasta":1.49,
-        "Lamb":25.44,
-        "Butter":11.52,
-        "Turkey":5.9,
-        "Rabbit":3.61,
-        "Cod":3.49,
-        "Eggs":3.4,
-        "Insect":2.3,
-        "Whole Milk":1.4,
-        "Wheat":0.51,
-        "Peas":0.6,
-        "Barley":0.52,
-        "Chickpeas":0.67}
